8ba46b2373591cb4e6eafc08270f5ce3353dab5e
siddhantkourav <siddhant2391@gmail.com>
2021-03-23 21:38:38 +0530

    first

---
+++ RoleBasedBasicAuthenticationWEBAPI/Models/UserBL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoleBasedBasicAuthenticationWEBAPI.Models
+{
+    public class UserBL
+    {
+        public List<User> GetUsers()
+        {
+            // In Realtime you need to get the data from any persistent storage
+            // For Simplicity of this demo and to keep focus on Basic Authentication
+            // Here we are hardcoded the data
+            List<User> userList = new List<User>();
+            userList.Add(new User()
+            {
+                ID = 101,
+                UserName = "AdminUser",
+                Password = "123456",
+                Roles = "Admin",
+                Email = "Admin@a.com"
+            });
+            userList.Add(new User()
+            {
+                ID = 102,
+                UserName = "BothUser",
+                Password = "abcdef",
+                Roles = "Admin,Superadmin",
+                Email = "BothUser@a.com"
+            });
+            userList.Add(new User()
+            {
+                ID = 103,
+                UserName = "SuperadminUser",
+                Password = "Password@123",
+                Roles = "Superadmin",
+                Email = "Superadmin@a.com"
+            });
+            return userList;
+        }
+    }
+}
---
+++ RoleBasedBasicAuthenticationWEBAPI/Models/UserValidate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoleBasedBasicAuthenticationWEBAPI.Models
+{
+    public class UserValidate
+    {
+        //This method is used to check the user credentials
+        public static bool Login(string username, string password)
+        {
+            UserBL userBL = new UserBL();
+            var UserLists = userBL.GetUsers();
+            return UserLists.Any(user =>
+                user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                && user.Password == password);
+        }
+        //This method is used to return the User Details
+        public static User GetUserDetails(string username, string password)
+        {
+            UserBL userBL = new UserBL();
+            return userBL.GetUsers().FirstOrDefault(user =>
+                user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+                && user.Password == password);
+        }
+    }
+}
